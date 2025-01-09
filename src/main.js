743460c5d177a44d759b1bdf999b618eae15c56d
nono951357 <berkileventemarko@gmail.com>
2025-01-09 10:04:04 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import App from './App.svelte';
-import { base } from '$app/paths';
+import { mount } from 'svelte'
+import './app.css'
+import App from './App.svelte'
+import { Router, Route } from 'svelte-routing'
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('app'),
   props: {
-    base: base || ''
+    Router,
+    Route
   }
-});
+})
 
-export default app;
+export default app
