12c39491565dff9fdc958d8e7d2620621a534725
nono951357 <berkileventemarko@gmail.com>
2025-01-09 10:22:17 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import { Router, Route } from 'svelte-routing'
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('app'),
+  props: {
+    Router,
+    Route
+  }
 })
 
 export default app
