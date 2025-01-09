10f1305f39a80e2f7edd23b053f1cc2b8574471b
nono951357 <berkileventemarko@gmail.com>
2025-01-09 09:54:50 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  // base: '/jatek_szimulacio/', // Replace <REPO_NAME> with your GitHub repository name
+  base: '/jatek_szimulacio/', // Replace <REPO_NAME> with your GitHub repository name
   build: {
     outDir: 'docs'
   }
